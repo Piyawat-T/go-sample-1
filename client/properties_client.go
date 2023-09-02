30ac7cb84921ece2d8973d0ff944d1467bb2370a
CS@ Spread BY <spreadby@businessmatching.co.uk>
2023-09-02 22:00:22 +0700

    Add transport

--- client/properties_client.go
+++ client/properties_client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"encoding/json"
 	"io"
+	"net/http"
 
 	"github.com/Piyawat-T/go-sample-1/domain"
 	"github.com/gin-gonic/gin"
@@ -17,13 +18,38 @@ func NewPropertiesClient() domain.PropertiesClient {
 	return &propertiesClient{}
 }
 
-func (client *propertiesClient) GetByApplicationAndProfile(c *gin.Context, application string, profile string) ([]domain.Properties, error) {
+func (pc *propertiesClient) GetByApplicationAndProfile(c *gin.Context, application string, profile string) ([]domain.Properties, error) {
 	ctx := c.Request.Context()
 	log := otelzap.L()
 	log.DebugContext(ctx, "Start Get Configuration")
 
-	var properties []domain.Properties
-	resp, err := otelhttp.Get(c, "http://localhost:8100/go-centralize-configuration/deposit/default")
+	// client := http.Client{
+	// 	Transport: otelhttp.NewTransport(
+	// 		http.DefaultTransport,
+	// 		otelhttp.WithClientTrace(func(ctx context.Context) *httptrace.ClientTrace {
+	// 			return otelhttptrace.NewClientTrace(ctx)
+	// 		}),
+	// 	),
+	// }
+	// resp, err := client.Get("http://localhost:8100/go-centralize-configuration/deposit/default")
+
+	// clientTrace := otelhttptrace.NewClientTrace(ctx)
+	// ctx = httptrace.WithClientTrace(ctx, clientTrace)
+
+	// tracer := otel.Tracer("hello-world-frontend")
+	// otel.SetTextMapPropagator(propagation.TraceContext{})
+	// ctx, span := tracer.Start(ctx, "client")
+	// defer span.End()
+
+	// span.AddEvent("Launching Request to backend")
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8100/go-centralize-configuration/deposit/default", nil)
+	if err != nil {
+		return nil, err
+	}
+	// resp, err := http.DefaultClient.Do(req)
+	httpclient := &http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+	resp, err := httpclient.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -32,6 +58,7 @@ func (client *propertiesClient) GetByApplicationAndProfile(c *gin.Context, appli
 	b, err := io.ReadAll(resp.Body)
 
 	log.DebugContext(ctx, resp.Status)
+	var properties []domain.Properties
 	json.Unmarshal(b, &properties)
 	return properties, err
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/spf13/viper v1.15.0
 	github.com/uptrace/opentelemetry-go-extra/otelzap v0.2.2
 	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.43.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace v0.43.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.43.0
 	go.opentelemetry.io/otel v1.17.0
 	go.opentelemetry.io/otel/sdk v1.15.1
